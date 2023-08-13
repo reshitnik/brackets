f37731422ecbe2a360d51e284410ffa23af0be3c
reshitnik <reshitnik@gmail.com>
2023-08-13 21:56:55 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    function checkStr(teststr) {
+    
+        let templ = "[]{}()|"
+        let stack = []
+      
+        for(let ibracket of teststr) {
+          let bracketsIndex = templ.indexOf(ibracket)
+          if(bracketsIndex % 2 === 0) {
+            stack.push(bracketsIndex +1 )
+              } else {
+            if(stack.pop() !== bracketsIndex) {
+              return false;
+            }
+          }
+        }
+        return stack.length === 0
+      }
+    
+      let str2 = bracketsConfig.join('').replaceAll(',','')
+      checkBracket1 = checkStr(str);
+      checkBracket2 =checkStr(str2);
+    
+      if (checkBracket1  == true & checkBracket2 ==true) {
+        return true;
+      } else {
+        return false
+    }
 }
