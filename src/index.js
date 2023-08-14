fb13c15a3baac7a2e54dbe3d0ce94db72bccbe62
reshitnik <reshitnik@gmail.com>
2023-08-14 18:00:08 +0300

    done v2.0

--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
-module.exports = function check(str, bracketsConfig) {
-    function checkStr(teststr) {
-    
-        let templ = "[]{}()|"
-        let stack = []
-      
-        for(let ibracket of teststr) {
-          let bracketsIndex = templ.indexOf(ibracket)
-          if(bracketsIndex % 2 === 0) {
-            stack.push(bracketsIndex +1 )
-              } else {
-            if(stack.pop() !== bracketsIndex) {
-              return false;
-            }
-          }
-        }
-        return stack.length === 0
+module.exports = function check(str1, bracketsConfig) {
+    function checkStr (str){
+      if (str.length %2 !==0){
+          return false
       }
+      else if (str.split().filter(i => i =='|').length %2 !== 0){
+          return false
+      }
+      let stack = [];
+      const bracketsOpen = '([{'
+
+      for (i=0; i<str.length; i++) {
+          if (bracketsOpen.includes(str[i])){
+              stack.push(str[i])
+              // console.log(stack)
+          } else if (str[i] ==')' & stack.slice(-1) =='(' || str[i] ==']' &  stack.slice(-1) =='[' || str[i] =='}' &  stack.slice(-1) =='{' || str[i] =='|' &  stack.slice(-1) =='|'){
+              // console.log(`Что в стеке - ${stack.slice(-1)}, что на итерации - ${str[i]}`)
+              stack.pop();
+              // console.log(stack)
+          } else {stack.push(str[i]) } 
+          
+      }
+      
+      return stack.length==0;
+    }
     
       let str2 = bracketsConfig.join('').replaceAll(',','')
-      checkBracket1 = checkStr(str);
+      checkBracket1 = checkStr(str1);
+      console.log(checkStr(str1))
       checkBracket2 =checkStr(str2);
+      console.log(checkStr(str2))
     
       if (checkBracket1  == true & checkBracket2 ==true) {
         return true;
